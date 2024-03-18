d975847d776afb7b730530d397536618574a6e1c
Filipe Hanniel <filipehanniel@gmail.com>
2024-03-17 23:28:08 -0300

    Projeto RESTful Web SpringBoot

---
+++ src/main/java/com/example/restful_web/Greeting.java
@@ -0,0 +1,5 @@
+package com.example.restful_web;
+
+public record Greeting(long id, String content) {
+    
+}
---
+++ src/test/java/com/example/restful_web/RestfulWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restful_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
